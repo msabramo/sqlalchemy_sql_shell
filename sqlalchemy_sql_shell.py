27155906a392fc987c18d409b6941d0f50c70c60
Marc Abramowitz <marca@surveymonkey.com>
2013-01-03 12:39:03 -0800

    Add SurveyMonkey to copyright

--- sqlalchemy_sql_shell.py
+++ sqlalchemy_sql_shell.py
@@ -6,7 +6,7 @@ to connect to different database engines.
 
 """
 
-# Copyright (c) 2012 Marc Abramowitz
+# Copyright (c) 2012-2013 Marc Abramowitz and SurveyMonkey
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of
 # this software and associated documentation files (the "Software"), to deal in
